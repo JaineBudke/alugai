2a2c0b85a4f11f60c980de2415837615ac21a549
jainebudke <jainebudke@hotmail.com>
2019-10-23 14:53:30 -0300

    implementacao frontend

---
+++ src/test/java/br/ufrn/aluguelImoveis/AluguelImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.aluguelImoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
