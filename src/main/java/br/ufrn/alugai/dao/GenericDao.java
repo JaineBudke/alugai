932629ef8567e272d8141e6fe4ab5cb497a318be
Mayra Azevedo <mayradazevedo@gmail.com>
2019-11-06 10:38:17 -0300

    add conexao com banco e entity manager

---
+++ src/main/java/br/ufrn/alugai/dao/GenericDao.java
@@ -0,0 +1,51 @@
+package br.ufrn.alugai.dao;
+
+import javax.persistence.EntityManager;
+
+public class GenericDao<T>  {
+	 
+	/**
+     * Salva a entidade no BD.
+     */
+    public void save(Object obj) {
+		EntityManager em = Banco.getEntityManager();
+        em.getTransaction()
+            .begin();
+        em.persist(obj);
+        em.getTransaction()
+            .commit();
+    }
+    
+    /**
+     * Atualiza a entidade no BD.
+     */
+    public void update(Object obj) {
+		EntityManager em = Banco.getEntityManager();
+        em.getTransaction()
+            .begin();
+        em.merge(obj);
+        em.getTransaction()
+            .commit();
+    }
+    
+    /**
+     * Remove a entidade no BD.
+     */
+    public void delete(Object obj) {
+		EntityManager em = Banco.getEntityManager();
+        em.getTransaction()
+            .begin();
+        em.remove(obj);
+        em.getTransaction()
+            .commit();
+    }
+    
+    /**
+     * Busca a entidade no BD.
+     */
+    public Object findById(Class entityClass, long id) {
+		EntityManager em = Banco.getEntityManager();
+        return em.find(entityClass, id);
+    }
+
+}
--- src/main/java/br/ufrn/alugai/model/Cliente.java
+++ src/main/java/br/ufrn/alugai/model/Cliente.java
@@ -1,5 +1,6 @@
 package br.ufrn.alugai.model;
 
+import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +9,11 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Polymorphism;
+import org.hibernate.annotations.PolymorphismType;
 
 @Entity
 @Table(name = "clientes")
@@ -21,7 +26,8 @@ public class Cliente extends Usuario {
 	private static final long serialVersionUID = -7088997936960839073L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_CLIENTE")
+    @SequenceGenerator(name="SEQ_CLIENTE", sequenceName = "SEQ_CLIENTE", allocationSize = 1)
 	private Integer id;
 	
 	@Column(name = "recebe_conteudo")
